aaefaaa485e695ea9d70ed5a2c0bb987781bd336
jtritschler2021 <jeanyvestritschler@gmail.com>
2022-03-16 09:34:38 +0100

    commit pour pouvoir pull

--- eniSortie/src/Controller/PrincipalController.php
+++ eniSortie/src/Controller/PrincipalController.php
@@ -2,43 +2,29 @@
 
 namespace App\Controller;
 
+use App\Form\model\FilterSearch;
 use App\Repository\ActivityRepository;
 use App\Repository\StatusRepository;
-use App\Form\model\FilterSearch;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PrincipalController extends AbstractController
 {
+    
 
+    /**
+     * @Route("/research", name="filter_status")
+     */
+     public function filterByStatus(ActivityRepository $activityRepository , StatusRepository $statusRepository): Response
+     {
+         $status= $statusRepository->findOneBy(['wording'=>'Créée']);
+        // $activity = $activityRepository->findBy(array('status'=>$statusRepository->findOneBy('Créée')));
 
-//    /**
-//     * @Route("/research", name="filter_status")
-//     */
-//    public function filterByStatus(ActivityRepository $activityRepository , StatusRepository $statusRepository): Response
-//    {
-//        $status= $statusRepository->findOneBy(['wording']);
-//
-//        return $this->render('principal/research.html.twig', [
-//            'status' => $activityRepository->findAll(),
-//            'titre'=>'Liste des activités',
-//        ]);
-//    }
-//
-//    /**
-//     * @Route("/research", name="filter_status")
-//     */
-//     public function filterByStatus(ActivityRepository $activityRepository , StatusRepository $statusRepository): Response
-//     {
-//         $status= $statusRepository->findOneBy(['wording'=>'Créée']);
-//        // $activity = $activityRepository->findBy(array('status'=>$statusRepository->findOneBy('Créée')));
-//
-//         return $this->render('principal/research.html.twig', [
-//             'status' => $activityRepository->findAll(),
-//             'titre'=>'Liste des activités',
-//         ]);
-//     }
-//
+         return $this->render('principal/research.html.twig', [
+             'status' => $activityRepository->findAll(),
+             'titre'=>'Liste des activités',
+         ]);
+     }
 
 }
